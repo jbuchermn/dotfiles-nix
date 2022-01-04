57c09e10508e34914f60099883cfeb51a1fd535a
Jonas Bucher <j.bucher.mn@gmail.com>
2022-01-04 17:50:19 +0100

    Alacritty

--- users/common/default.nix
+++ users/common/default.nix
@@ -41,4 +41,30 @@
       vim-nix
     ]);
   };
+
+  programs.alacritty = {
+    enable = true;
+  };
+  xdg.configFile."alacritty/alacritty.yml".text = ''
+font:
+    normal:
+        family: Source Code Pro for Powerline
+        style: Regular
+    bold:
+        family: Source Code Pro for Powerline
+        style: Bold
+    italic:
+        family: Source Code Pro for Powerline
+        style: Italic
+    bold_italic:
+        family: Source Code Pro for Powerline
+        style: Bold Italic
+    size: 8
+
+background_opacity: 0.9
+  '';
+
+  home.packages = [
+    pkgs.powerline-fonts
+  ];
 }
