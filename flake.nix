aee94d34aee2375b2ac57a46a1636fa0e07a47c9
Jonas Bucher <j.bucher.mn@gmail.com>
2022-01-05 23:25:07 +0100

    Move to newm flakes

--- flake.nix
+++ flake.nix
@@ -2,14 +2,19 @@
   description = "jbuchermn system config";
 
   inputs = {
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs.url = "github:jbuchermn/nixpkgs/master";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nur.url = "github:nix-community/NUR";
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    newm-pkg.url = "github:jbuchermn/newm/4686f5d";
+    newm-pkg.inputs.nixpkgs.follows = "nixpkgs";
+
+    # newm-pkg.url = "path:/home/jonas/newm-dev/pywm";
+    # newm-pkg.inputs.pywm.url = "path:/home/jonas/newm-dev/newm";
   };
 
-  outputs = { nixpkgs, nur, home-manager, ... }:
+  outputs = { nixpkgs, nur, home-manager, newm-pkg, ... }:
   let
     system = "x86_64-linux";
     stateVersion = "22.05";
@@ -20,6 +25,7 @@
       };
       overlays = [
         nur.overlay
+        (final: prev: { newm = newm-pkg.packages.${system}.newm; })
       ];
     };
     lib = nixpkgs.lib;
