8c247ccb96d60af0828577bcef2eeba200e34b1b
Jonas Bucher <j.bucher.mn@gmail.com>
2022-01-13 12:59:04 +0100

    neomutt working

--- users/jonas/home.nix
+++ users/jonas/home.nix
@@ -19,57 +19,6 @@
     ../common/alacritty.nix
     ../common/nvim.nix
     ../common/newm.nix
+    ../common/neomutt.nix
   ];
-
-  programs.neomutt = {
-    enable = true;
-    vimKeys = true;
-  };
-  programs.notmuch = {
-    enable = true;
-    hooks = {
-      preNew = "mbsync --all";
-    };
-  };
-  programs.mbsync = {
-    enable = true;
-  };
-
-  accounts.email.accounts.jbuchermn-gmail = {
-    realName = "Jonas Bucher";
-    signature.text = ''
-      Viele Grüße
-      Jonas
-    '';
-
-    primary = true;
-    address = "j.bucher.mn@gmail.com";
-    passwordCommand = "pass show gmail";
-
-    userName = "j.bucher.mn@gmail.com";
-    imap = {
-      host = "imap.gmail.com";
-      port = 993;
-      tls.useStartTls = false;
-    };
-
-    smtp = {
-      host = "smtp.gmail.com";
-      port = 465;
-    };
-
-    mbsync = {
-      enable = true;
-      patterns = [
-        "*" "!\"[Gmail]/All Mail\"" 
-        "!\"[Gmail]/Important\"" 
-        "!\"[Gmail]/Starred\""
-        "!\"[Gmail]/Bin\""
-      ];
-      create = "both";
-      expunge = "both";
-    };
-    notmuch.enable = true;
-    neomutt.enable = true;
-  };
 }
